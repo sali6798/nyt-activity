c100da2c7623961d1c9a245912b26658729effa2
Diana Stebbins <dianastebbins97@gmail.com>
2020-03-20 13:22:09 -0700

    working on search parameters

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&page=2&sort=newest&api-key=PAnMpY7tfyyQehAvMDq36XwlKpK4RQAK";
 
     var api_key = "&api-key=PAnMpY7tfyyQehAvMDq36XwlKpK4RQAK";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&begin_date=20191031&end_date=20191102" + api_key;
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&begin_date=20191031&end_date=20191102&q=trump&limit=5" + api_key;
 
     $.ajax({
         url: queryURL,
