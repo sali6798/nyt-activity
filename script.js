4be879a5970691e1cb8d9121a2ffd68dfe79840c
Diana Stebbins <dianastebbins97@gmail.com>
2020-03-20 13:07:53 -0700

    initial API call

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    // example on nyt page
+    //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&page=2&sort=newest&api-key=PAnMpY7tfyyQehAvMDq36XwlKpK4RQAK";
+
+    var api_key = "&api-key=PAnMpY7tfyyQehAvMDq36XwlKpK4RQAK";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&begin_date=20191031&end_date=20191102" + api_key;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+        });
     
 });
